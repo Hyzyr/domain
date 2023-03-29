b18b0760125d796e81f11d22fe5cf4dc53d4b328
Hyzyr <avezberdiyew@gmail.com>
2023-03-29 20:55:17 +0500

    sdsd

--- js/main.js
+++ js/main.js
@@ -63,12 +63,82 @@ const slickSettings = {
       {
         breakpoint: 460,
         settings: {
+          slidesToShow: 2,
+          cssEase: "linear",
+          speed: 2000,
+          autoplay: true,
+          autoplaySpeed: 0,
+        },
+      },
+    ],
+  },
+  default: {
+    slidesToShow: 3,
+    speed: 1500,
+    arrows: true,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2.5,
+          speed: 1000,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 2,
+          speed: 1000,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 1.6,
+          speed: 1000,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1.2,
+          speed: 1000,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 460,
+        settings: {
           slidesToShow: 1,
           speed: 1000,
           autoplay: true,
         },
       },
     ],
+  },
+  feedbacksImages: {
+    slidesToShow: 1,
+    speed: 300,
+    arrows: false,
+    dots: false,
+    fade: true,
+    cssEase: "ease-out",
+    asNavFor: "#feedbacksContent",
+  },
+
+  feedbacksContent: {
+    slidesToShow: 1,
+    speed: 0,
+    arrows: false,
+    dots: true,
+    appendDots: $("#feedbacksContentDots"),
+    fade: true,
+    cssEase: "linear",
+    asNavFor: "#feedbacksImages",
   },
 };
 
